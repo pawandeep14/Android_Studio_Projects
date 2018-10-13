4d3737ed1d942fec7631f9a5dd809761271c5809
Pawandeep Singh Nanda <2016pawandeep.nanda@ves.ac.in>
2018-10-13 23:11:45 +0530

    Commit on 13 oct 11:11 pm

--- app/src/main/java/com/example/karan/myapplication/Home_NGO.java
+++ app/src/main/java/com/example/karan/myapplication/Home_NGO.java
@@ -1,13 +1,63 @@
 package com.example.karan.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Home_NGO extends AppCompatActivity {
+public class Home_NGO extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__ngo);
+        ImageButton createevent,edit_details,view_activist;
+        createevent=(ImageButton) findViewById(R.id.createevent);
+        edit_details=(ImageButton) findViewById(R.id.editdetails);
+        view_activist=(ImageButton) findViewById(R.id.viewactivists);
+
+        createevent.setOnClickListener(this);
+        edit_details.setOnClickListener(this);
+        view_activist.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId())
+        {
+            case R.id.createevent:
+            {
+
+                Intent i=new Intent(this,Create_Event.class);
+                startActivity(i);
+
+
+            }
+            break;
+            case R.id.editdetails:
+            {
+
+            }
+            break;
+            case R.id.viewactivists:
+            {
+
+            }
+            break;
+            default:
+
+
+
+
+
+
+
+        }
+
     }
 }
