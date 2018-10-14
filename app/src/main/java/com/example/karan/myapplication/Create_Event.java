b0f56ca3c8064611a23192cadf981fefd1eff50d
Pawandeep Singh Nanda <2016pawandeep.nanda@ves.ac.in>
2018-10-14 12:48:31 +0530

    Commit on 14 oct 12:48 pm

--- app/src/main/java/com/example/karan/myapplication/Create_Event.java
+++ app/src/main/java/com/example/karan/myapplication/Create_Event.java
@@ -2,12 +2,34 @@ package com.example.karan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Create_Event extends AppCompatActivity {
+public class Create_Event extends AppCompatActivity implements View.OnClickListener {
+
+    Button addevent;
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create__event);
+    addevent=(Button) findViewById(R.id.addeventbutton);
+    addevent.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
     }
 }
